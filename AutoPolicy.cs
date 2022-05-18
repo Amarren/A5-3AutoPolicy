f315a0e80261cb9aa9cb873c402e584f69cab3e1
4400112062 <4400112062@BT144-PROG09>
2022-05-17 19:29:10 -0600

    Add project files.

---
+++ AutoPolicy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AutoPolicy
+{
+    class AutoPolicy
+    {
+        public int AccountNumber { get; set; } //policy account number
+        public string MakeAndModel { get; set; } //car that policy applies to
+        public string State { get; set; } //two-letter state abbreviation
+
+        bool noFaultState;
+
+        //Constructor
+        public AutoPolicy(int accountNumber, string makeAndModel, string state)
+        {
+            AccountNumber = accountNumber;
+            MakeAndModel = makeAndModel;
+            State = state;
+
+        }
+
+        //returns whether the state has no-fault insurance
+        public bool IsNoFaultState
+        {
+            get
+            {
+
+                if ((State == "CT") || (State == "MA") || (State == "ME") || (State == "NH") || (State == "NJ") || (State == "NY") || (State == "PA") || (State == "VT"))
+                {
+                    noFaultState = true;
+                    return noFaultState;
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR: Not Acceptable State");
+                    noFaultState = false;
+                    return noFaultState;
+
+
+                }
+            }
+        }
+    }
+}
---
+++ AutoPolicyTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPolicy
+{
+    class AutoPolicyTest
+    {
+        static void Main()
+        {
+            //create two AutoPPolicy Objects
+            AutoPolicy policy1 = new AutoPolicy(11111111, "Toyota Camry", "UT");
+            AutoPolicy policy2 = new AutoPolicy(22222222, "Ford Fusion", "MA");
+
+
+
+            //display wether each policy is in a no-fault state
+            PolicyInNoFaultState(policy1);
+            PolicyInNoFaultState(policy2);
+        }
+
+        //method that displays whether an AutoPolicy
+        //is in a state with no-fault auto insurance
+        public static void PolicyInNoFaultState(AutoPolicy policy)
+        {
+            Console.WriteLine("The auto Policy:");
+            Console.Write($"Account #: {policy.AccountNumber}; ");
+            Console.WriteLine($"Car: {policy.MakeAndModel};");
+            Console.Write($"State {policy.State}; ");
+            Console.Write($"({(policy.IsNoFaultState ? "is" : "is not")})");
+            Console.WriteLine(" a no-fault State\n");
+        }
+    }
+}
